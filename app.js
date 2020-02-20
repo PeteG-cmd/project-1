f11635ea08c394a2856cb5c8db71fa58334d27df
Peter Gilbert <peter.gilbert88@icloud.com>
2020-02-20 16:56:17 +0000

    Ghosts going through transport tunnel issues handled

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ function main() {
   const gridCellCount = width * width
   const grid = document.querySelector('.grid')
   const cells = []
+  let score = 0
   let dude = 250
   let ghost1 = 209
   let ghost2 = 211
 
   const wallCells = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 29, 30, 39, 40, 42, 43, 45, 46, 47, 49, 50, 52, 53, 54, 56, 57, 59, 60, 62, 63, 65, 66, 67, 69, 70, 72, 73, 74, 76, 77, 79, 80, 99, 100, 102, 103, 105, 107, 108, 109, 110, 111, 112, 114, 116, 117, 119, 120, 125, 129, 130, 134, 139, 140, 141, 142, 143, 145, 146, 147, 149, 150, 152, 153, 154, 156, 157, 158, 159, 160, 161, 162, 163, 165, 174, 176, 177, 178, 179, 180, 181, 182, 183, 185, 187, 188, 189, 190, 191, 192, 194, 196, 197, 198, 199, 207, 212, 220, 221, 222, 223, 225, 227, 228, 229, 230, 231, 232, 234, 236, 237, 238, 239, 240, 245, 254, 259, 260, 262, 263, 265, 267, 268, 269, 270, 271, 272, 274, 276, 277, 279, 280, 283, 296, 299, 300, 301, 303, 303, 305, 306, 307, 309, 310, 312, 313, 314, 316, 318, 319, 320, 325, 329, 330, 334, 339, 340, 342, 343, 345, 347, 349, 350, 352, 354, 356, 357, 359, 360, 367, 372, 379, 380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399]
+
+  const noFoodCells = [166, 167, 168, 169, 170, 171, 172, 173, 200, 208, 209, 210, 211, 219, 246, 247, 248, 249, 250, 251, 252, 253]
 
   for (let i = 0; i < gridCellCount; i++) {
     const cell = document.createElement('div')
@@ -17,6 +20,9 @@ function main() {
     cell.innerHTML = i // REMOVE ONCE FINISHED WITH NUMBERS
     if (i === dude) {
       cell.classList.add('dude')
+    }
+    if (!(noFoodCells.includes(i)) && (!(wallCells.includes(i)))) {
+      cell.classList.add('food')
     }
     if (i === ghost1) {
       cell.classList.add('ghost1')
@@ -34,8 +40,17 @@ function main() {
   }
 
 
+
+
   // THIS CODE CONTROLLS THE MOVEMENT OF THE PLAYER AND DOES NOT ALLOW MOVEMENT IN TO WALLS
+
+
+
   document.addEventListener('keydown', (event) => {
+
+
+
+
     if (event.key === 'ArrowRight') {
       if (dude === 219) {
         cells[dude].classList.remove('dude')
@@ -48,6 +63,7 @@ function main() {
       cells[dude].classList.remove('dude')
       dude += 1
       cells[dude].classList.add('dude')
+      removeFoodIncrementScore(dude)
     } else if (event.key === 'ArrowLeft') {
       if (dude === 200) {
         cells[dude].classList.remove('dude')
@@ -59,6 +75,7 @@ function main() {
       cells[dude].classList.remove('dude')
       dude -= 1
       cells[dude].classList.add('dude')
+      removeFoodIncrementScore(dude)
     } else if (event.key === 'ArrowDown') {
       if (wallCells.includes(dude + 20)) {
         return
@@ -66,6 +83,7 @@ function main() {
       cells[dude].classList.remove('dude')
       dude += 20
       cells[dude].classList.add('dude')
+      removeFoodIncrementScore(dude)
     } else if (event.key === 'ArrowUp') {
       if (wallCells.includes(dude - 20)) {
         return
@@ -73,8 +91,13 @@ function main() {
       cells[dude].classList.remove('dude')
       dude -= 20
       cells[dude].classList.add('dude')
+      removeFoodIncrementScore(dude)
     }
+
+
   })
+
+
 
 
   //LOGIC TO ACTIVATE AND CONTROLL GHOST
@@ -82,7 +105,7 @@ function main() {
   let ghost1Direction // THIS STOPS GHOST 1 REVERSING DIRECTION FOR NO REASON
   let ghost2Direction // THIS STOPS GHOST 2 REVERSING DIRECTION FOR NO REASON
 
-  setInterval(() => {
+  const intervalId = setInterval(() => {
 
     //GHOST 1 CONTROLL
     // THIS CODE CALCULATES THE AVALABLE CELLS TO MOVE TO IN TO AN ARRAY AND PICKS A RANDOM NUMBER TO CHOOSE THE DIRECTION FROM AVAILABLE
@@ -101,14 +124,22 @@ function main() {
       ghost1AvailableDirctions.push(ghost1 - 20)
     }
 
-    if (ghost1 !== 209) {
-      
+    // THE FOLLOWING 2 X IF ELSE HANDLE IF THE GHOST GOES THROUGH THE TRANSPORT TUNNEL
+    if (ghost1 === 219 && ghost1Direction === 2) {
+      cells[ghost1].classList.remove('ghost1')
+      ghost1 = 200
+      cells[ghost1].classList.add('ghost1')
+    } else if (ghost1 === 200 && ghost1Direction === 4) {
+      cells[ghost1].classList.remove('ghost1')
+      ghost1 = 219
+      cells[ghost1].classList.add('ghost1')
+    } else if (ghost1 !== 209) {
+
       // THIS PICHS THE DIRECTION A GHOST SHOULD GO AT AN INTERSECTION AND IMPLEMENTS IT
       const nextCellGhost = Math.floor((Math.random()) * ghost1AvailableDirctions.length)
-      console.log(nextCellGhost)
 
       ghost1Direction = directionMoving(ghost1, ghost1AvailableDirctions[parseInt(nextCellGhost)])
-      console.log(ghost1Direction)
+
 
       cells[ghost1].classList.remove('ghost1')
       ghost1 = ghost1AvailableDirctions[parseInt(nextCellGhost)]
@@ -132,16 +163,30 @@ function main() {
       ghost2AvailableDirctions.push(ghost2 - 20)
     }
 
-    if (ghost2 !== 211) {
-      
+    // THE FOLLOWING 2 X IF ELSE HANDLE IF THE GHOSTS GO THROUGH THE TRANSPORT TUNNEL
+    if (ghost2 === 219 && ghost2Direction === 2) {
+      cells[ghost2].classList.remove('ghost2')
+      ghost2 = 200
+      cells[ghost2].classList.add('ghost2')
+    } else if (ghost2 === 200 && ghost2Direction === 4) {
+      cells[ghost2].classList.remove('ghost2')
+      ghost2 = 219
+      cells[ghost2].classList.add('ghost2')
+    } else if (ghost2 !== 211) {
+
       // THIS PICHS THE DIRECTION A GHOST SHOULD GO AT AN INTERSECTION AND IMPLEMENTS IT
       const nextCellGhost = Math.floor((Math.random()) * ghost2AvailableDirctions.length)
-      console.log(nextCellGhost)
 
       ghost2Direction = directionMoving(ghost2, ghost2AvailableDirctions[parseInt(nextCellGhost)])
 
       cells[ghost2].classList.remove('ghost2')
-      ghost2 = ghost2AvailableDirctions[parseInt(nextCellGhost)]
+      if (ghost2 === 200 && ghost2Direction === 4) {
+        ghost2 = 219
+      } else if (ghost2 === 219 && ghost2Direction === 2) {
+        ghost2 = 200
+      } else {
+        ghost2 = ghost2AvailableDirctions[parseInt(nextCellGhost)]
+      }
       cells[ghost2].classList.add('ghost2')
     }
 
@@ -159,7 +204,18 @@ function main() {
       cells[ghost2].classList.add('ghost2')
     }
 
-  }, 1000)
+    console.log(intervalId)
+
+
+  }, 500)
+
+
+  function removeFoodIncrementScore(cellNum) {
+    if (cells[cellNum].classList.contains('food')) {
+      cells[cellNum].classList.remove('food')
+      score += 10
+    }
+  }
 
 
   function directionMoving(currentCell, newCell) {
@@ -176,7 +232,6 @@ function main() {
       return 3
     }
   }
-
 }
 
 
